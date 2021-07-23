ba796c5e1160c0127158e59fcd2252f5af6e4d71
Mariane <nani_karoline@hotmail.com>
2021-07-23 11:22:19 -0300

    Exercicio de etiquetas de produtos

---
+++ ExercicioEtiquetas/Entities/ImportedProduct.cs
@@ -0,0 +1,29 @@
+﻿using System.Globalization;
+
+namespace ExercicioEtiquetas.Entities
+{
+    class ImportedProduct : Product
+    {
+        public double CustomsFee { get; set; }
+
+        public ImportedProduct()
+        {
+
+        }
+
+        public ImportedProduct(string name, double price, double customnsFee) : base(name, price)
+        {
+            CustomsFee = customnsFee;
+        }
+
+        public double TotalPrice()
+        {
+            return Price + CustomsFee;
+        }
+
+        public override string PriceTag()
+        {
+            return Name + " $" + TotalPrice().ToString("F2", CultureInfo.InvariantCulture) + " (Customs fee: $" + CustomsFee + ")";
+        }
+    }
+}
---
+++ ExercicioEtiquetas/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using ExercicioEtiquetas.Entities;
+
+namespace ExercicioEtiquetas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> products = new List<Product>();
+            Console.Write("Enter the number of products: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"\nProduct #{i} data:");
+                Console.Write("Common, Used or Imported (c/u/i)? ");
+                char op = char.Parse(Console.ReadLine());
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Price: ");
+                double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                switch (op)
+                {
+                    case 'c':
+                        products.Add(new Product(name, price));
+                        break;
+                    case 'i':
+                        Console.Write("Customs fee: ");
+                        double customsFee = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                        products.Add(new ImportedProduct(name, price, customsFee));
+                        break;
+                    case 'u':
+                        Console.Write("Manufacture date (DD/MM/YYYY): ");
+                        DateTime manufactureDate = DateTime.Parse(Console.ReadLine());
+
+
+                        products.Add(new UsedProduct(name, price, manufactureDate));
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            Console.WriteLine("\n\nPRICE TAGS: ");
+            foreach (var item in products)
+            {
+                Console.WriteLine(item.PriceTag());
+            }
+        }
+    }
+}
